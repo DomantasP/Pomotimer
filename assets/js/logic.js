6d8526ac9d7d8e128f6d16e9070f541c116eb103
Domantas Pelaitis <domantas.pelaitis@gmail.com>
2016-12-16 22:32:47 +0200

    Adding cookies support

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -76,26 +76,67 @@ var Profile = function(workTime,shortTime,longTime){
 	this.shortTime = shortTime;
 	this.longTime = longTime;
 	this.shortBreak = 0;
-	this.pomodoros = 0;
-	this.elapsed = 0;
+	this.pomodoros = (readCookie('pomodoros') != null ) ? parseInt(getCookie('pomodoros')) : 0;
+	this.elapsed = (readCookie('elapsed') != null ) ? parseInt(getCookie('elapsed')) : 0;
 	this.isBreak = true;
 };
 
-//cia padaryti, kad gal is cookie prima nusiskaito
-var profile = new Profile(25,5,15);
-var time = 25 * 60;
+function createCookie(name,value,days) {
+    if (days) {
+        var date = new Date();
+        date.setTime(date.getTime() + (days*24*60*60*1000));
+        var expires = "; expires=" + date.toUTCString();
+    }
+    else var expires = "";
+    document.cookie = name + "=" + value + expires + "; path=/";
+}
+
+Profile.prototype.setPomodoroCookies = function(){
+	var oneDay = new Date();
+	oneDay.setTime(oneDay.getTime() + (24*60*60*1000))
+	var expires = ';expires=' + oneDay.toUTCString();
+	document.cookie = 'pomodoros=' + this.pomodoros + ';' + expires + ';path=/';
+	document.cookie = 'elapsed=' + this.elapsed + ';' + expires + ';path=/';
+}
+
+function readCookie(name) {
+    var nameEQ = name + "=";
+    var ca = document.cookie.split(';');
+    for(var i=0;i < ca.length;i++) {
+        var c = ca[i];
+        while (c.charAt(0)==' ') c = c.substring(1,c.length);
+        if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length,c.length);
+    }
+    return null;
+}
+
+
+var wTime = (readCookie('workTime') != null) ? parseInt(readCookie('workTime')) :25 ;
+console.log(readCookie('workTime'));
+var sTime = (readCookie('shortTime') != null) ? parseInt(readCookie('shortTime')) : 5;
+console.log(readCookie('shortTime'));
+var lTime = (readCookie('longTime') != null) ? parseInt(readCookie('longTime')) : 15;
+console.log(readCookie('longTime'));
+
+var profile = new Profile(wTime,sTime,lTime);
+
+var time = profile.workTime * 60;
 var mainClock = $('#main-time');
 var secondaryClock = $('#done-time');
 var titleClock = $('title');
 var mainTimer = new Timer(mainClock,titleClock);
 var audioElement = document.createElement('audio');
-audioElement.setAttribute('src', 'assets/sounds/doneSound.mp3');
+audioElement.setAttribute('src', 'assets/sounds/doneSound.mp3');	
 
 
 $('#save-settings').click(function(){
 	profile.workTime = isNaN( parseInt($('#workTime').val() ) ) ? profile.workTime : parseInt($('#workTime').val()) ;
 	profile.shortTime = isNaN( parseInt($('#shortTime').val()) ) ? profile.shortTime : parseInt($('#shortTime').val()) ;
 	profile.longTime = isNaN( parseInt($('#longTime').val()) ) ? profile.longTime : parseInt($('#longTime').val());
+	
+	createCookie('workTime',profile.workTime,30);
+	createCookie('shortTime',profile.shortTime,30);
+	createCookie('longTime',profile.longTime,30);
 
 	$('#workTime').attr('placeholder',profile.workTime);
     $('#shortTime').attr('placeholder',profile.shortTime);
@@ -117,6 +158,8 @@ var updateOnBreak = function(){
 			profile.pomodoros++;
 			$('.tomato-row').append('<img src="assets/img/tomato-svg.svg" alt="tomato-icon">');	
 			$('#main-tomato').css('box-shadow', '0px 0px 15px 3px #00B16A');
+			createCookie('pomodoros',profile.pomodoros,1);
+			createCookie('elapsed',profile.elapsed,1);
 }
 
 window.addEventListener('finished', function(e){
